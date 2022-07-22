0e113e00890bc807d1c84f5c73c9041fe47156a8
Seongbum Seo <seo@seongbum.com>
2022-07-22 10:40:33 +0900

    Add shell scripts to run train and evaluation

---
+++ src/question-answering/korquad2/run_squad.sh
@@ -0,0 +1,11 @@
+python run_qa.py \
+  --model_name_or_path bert-base-uncased \
+  --dataset_name squad \
+  --do_train \
+  --do_eval \
+  --per_device_train_batch_size 12 \
+  --learning_rate 3e-5 \
+  --num_train_epochs 2 \
+  --max_seq_length 384 \
+  --doc_stride 128 \
+  --output_dir /tmp/debug_squad/
