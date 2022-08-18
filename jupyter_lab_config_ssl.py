eb5beddd7b92ecefbe70c2ae85c635ba3a21e377
Seongbum Seo <seo@seongbum.com>
2022-08-18 23:25:39 +0000

    Separate options based on whether SSL is enabled or not

--- jupyter_lab_config_ssl.py
+++ jupyter_lab_config_ssl.py
@@ -5,4 +5,8 @@ c.ServerApp.password_required = True
 c.ServerApp.password = 'argon2:$argon2id$v=19$m=10240,t=10,p=8$BaLOJQXLSsOjQ9Qu8LxBwA$Jj5/AH6T0uQbMd4Nr18chbQU9fL7lZiUCFlEP4d00Rg'
 c.ServerApp.open_browser = False
 c.ServerApp.ip = '0.0.0.0'
-#c.ServerApp.port = 443
+c.ServerApp.port = 443
+
+# SSL
+c.ServerApp.certfile = '/root/.ssl/cert1.pem'
+c.ServerApp.keyfile = '/root/.ssl/privkey1.pem'
