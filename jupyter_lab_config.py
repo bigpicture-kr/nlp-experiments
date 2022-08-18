627dee674df01a1acfa21a5a0275f2e4453942ac
Seongbum Seo <seo@seongbum.com>
2022-08-18 16:13:56 +0900

    Update jupyter_lab_config.py

--- jupyter_lab_config.py
+++ jupyter_lab_config.py
@@ -1,6 +1,6 @@
 c.ServerApp.allow_root = True
 c.ServerApp.allow_origin = '*'
-c.ServerApp.root_dir = '/gpt'
+c.ServerApp.root_dir = '/nlp'
 c.ServerApp.password_required = True
 c.ServerApp.password = 'argon2:$argon2id$v=19$m=10240,t=10,p=8$BaLOJQXLSsOjQ9Qu8LxBwA$Jj5/AH6T0uQbMd4Nr18chbQU9fL7lZiUCFlEP4d00Rg'
 c.ServerApp.open_browser = False
